8c1c6c0bdae3c3c43ff0f16c80ca1eeb3dbc905f
Jr002 <studises@gmail.com>
2019-06-26 11:09:32 +0800

    Login Page Comment

--- NPFoodOrdering/app/src/main/java/com/example/www/npfoodordering/LoginPage.java
+++ NPFoodOrdering/app/src/main/java/com/example/www/npfoodordering/LoginPage.java
@@ -25,6 +25,8 @@ public class LoginPage extends AppCompatActivity {
             public void onClick(View v) {
                 nmee = nme.getText().toString();
                 passw = pass.getText().toString();
+
+                //Login code
                 if (nmee.equals("abc") & passw.equals("1234"))
                 {
                     Intent intent = new Intent(LoginPage.this, homepage.class);
