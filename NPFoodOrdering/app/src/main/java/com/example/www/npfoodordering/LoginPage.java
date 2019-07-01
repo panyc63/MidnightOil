ef24cf5a92e069856d4bc1d0bb267d1c02a13f1a
panyc63 <panyc63@gmail.com>
2019-07-01 18:39:01 +0800

    added QR Scanner
    Signed-off-by: panyc63 <panyc63@gmail.com>

--- NPFoodOrdering/app/src/main/java/com/example/www/npfoodordering/LoginPage.java
+++ NPFoodOrdering/app/src/main/java/com/example/www/npfoodordering/LoginPage.java
@@ -16,7 +16,7 @@ public class LoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
-        loginbtn = findViewById(R.id.button);
+        loginbtn = findViewById(R.id.btnLogin);
         nme = findViewById(R.id.editText);
         pass = findViewById(R.id.editText2);
         loginbtn.setOnClickListener(new View.OnClickListener() {
@@ -25,15 +25,13 @@ public class LoginPage extends AppCompatActivity {
             public void onClick(View v) {
                 nmee = nme.getText().toString();
                 passw = pass.getText().toString();
-
-                //Login Page
-                if (nmee.equals("abc") & passw.equals("1234"))
+                if (nmee.equals("admin") & passw.equals("1234"))
                 {
                     Intent intent = new Intent(LoginPage.this, homepage.class);
                     startActivity(intent);
                     finish();
                 } else {
-                    Toast.makeText(LoginPage.this, "Wrong Login Details", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginPage.this, "Wrong Login Details", Toast.LENGTH_LONG).show();
                 }
             }
         });
--- NPFoodOrdering/app/src/main/java/com/example/www/npfoodordering/homepage.java
+++ NPFoodOrdering/app/src/main/java/com/example/www/npfoodordering/homepage.java
@@ -1,13 +1,37 @@
 package com.example.www.npfoodordering;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class homepage extends AppCompatActivity {
+    private Button btnQR,btnLogout;
+    public static TextView output;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+        btnQR = findViewById(R.id.btnQR);
+        output = findViewById(R.id.tvOutput);
+        btnLogout = findViewById(R.id.btnLogout);
+
+        btnQR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(homepage.this, qr_scanner.class));
+
+            }
+        });
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(homepage.this,LoginPage.class));
+                finish();
+            }
+        });
     }
 }
